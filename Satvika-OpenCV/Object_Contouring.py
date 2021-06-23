bdab4b643f2c95cb11a2790bc753c6481ad9cf01
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-06-23 12:00:25 +0530

    Update Object_Contouring.py

--- Satvika-OpenCV/Object_Contouring.py
+++ Satvika-OpenCV/Object_Contouring.py
@@ -20,3 +20,22 @@ cv2.imshow('Thresh',thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
+'''
+Some importtant functions in OpenCV:
+
+•	cv2.threshold(src,thresholdvalue,maxvalue, thresholding_technique)
+Returns the threshold value(ret) and the thresholded frame. Thresholding technique can be cv2.THRESH_BINARY or cv2.THRESH_BINARY_INV.
+
+
+•	cv2.findContours(src,retrieval method,contour approximation method)
+Returns a list of all numpy arrays which contain the x,y co-ordinates of all boundary points.
+	Retrieval method – cv2.RETR_TREE or cv2.RETR_EXTERNAL
+	Contour approximation – cv2.CHAIN_APPROX_SIMPLE or 	cv2.CHAIN_APPROX_NONE.
+	Note: There are many more methods and they can be found in the official documentation.
+
+
+•	cv2.drawContours(src,contours,contour index,color,thickness)
+Draws the contours on our source image. Contours are returned from the cv2.findContours function given above. Contour index helps us in displaying only specific contours.
+
+'''
