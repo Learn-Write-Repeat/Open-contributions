c8520665672c495ed3573f7c860073cb4f6788f6
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-06-23 11:52:16 +0530

    Update Object_Contouring.py

--- Satvika-OpenCV/Object_Contouring.py
+++ Satvika-OpenCV/Object_Contouring.py
@@ -1,7 +1,12 @@
+# This code discussed about object contouring wherein an image consisting of different shapes is thresholded 
+# and then the outlines for each shape is identified using the openCV function findContours().
+# We are printing both thresholded and contoured images here.
+
+
 import numpy as np
 import cv2
 
-img  = cv2.imread('C:\Python\ProgramsPython\HelloWorld\\venv\OpenCV\Resources\Resources(Day-1)\Contours\Shapes.png')
+img  = cv2.imread('C:\Python\ProgramsPython\HelloWorld\\venv\OpenCV\Contours\Shapes.png')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 ret,thresh = cv2.threshold(gray , 220 ,255 , cv2.THRESH_BINARY_INV)
@@ -14,6 +19,4 @@ cv2.imshow('Image',img)
 cv2.imshow('Thresh',thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
 
